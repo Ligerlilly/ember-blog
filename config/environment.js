28f2b4fd1bfbee9361a526f30e076ba3cf049b14
Jason Lane & ryan broughan <student@epicodus.com>
2015-09-23 09:56:06 -0700

    Initial commit

--- config/environment.js
+++ config/environment.js
@@ -4,6 +4,8 @@ module.exports = function(environment) {
   var ENV = {
     modulePrefix: 'blog-app',
     environment: environment,
+    contentSecurityPolicy: { 'connect-src': "'self' https://auth.firebase.com wss://*.firebaseio.com" },
+    firebase: 'https://popping.firebaseio.com/',
     baseURL: '/',
     locationType: 'auto',
     EmberENV: {
